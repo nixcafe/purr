9756941affca687fab9b32a6312294b403a117ac
Sobte <i@sobte.me>
2026-07-14 17:15:27 +0800

    chore: remove unnecessary placeholder headers and empty imports (#6)

--- lib.nix
+++ lib.nix
@@ -1,16 +1,16 @@
 { lib }:
 let
-  systems = import ./lib/systems.nix { };
+  systems = import ./lib/systems.nix;
 
-  attrs = import ./lib/attrs.nix { };
+  attrs = import ./lib/attrs.nix;
 
-  fs = import ./lib/fs.nix { };
+  fs = import ./lib/fs.nix;
 
   modules = import ./lib/modules.nix {
     inherit fs lib;
   };
 
-  namespacedModules = import ./lib/namespacedModules.nix { };
+  namespacedModules = import ./lib/namespacedModules.nix;
 
   mkFlake = import ./lib/mkFlake.nix {
     inherit
--- lib/namespacedModules.nix
+++ lib/namespacedModules.nix
@@ -1,4 +1,3 @@
-_:
 let
   wrapModule =
     namespace: module: args:
