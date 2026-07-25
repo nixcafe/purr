b88bd9f2b0d3dc3455dd6268ddf4c37d634eec76
sobte <i@sobte.me>
2026-07-25 20:12:46 +0800

    chore: update dev flake inputs, use follows for git-hooks

--- dev/flake.nix
+++ dev/flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
